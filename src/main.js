aaae6b9f64bd66ff8e44acbbf104621099deb73d
Steve_Zhang <wentaozhang0305@gmail.com>
2020-06-03 20:28:43 -0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+//import
+import VueRouter from 'vue-router';
+//use
+Vue.use(VueRouter);
+//create
+let router = new VueRouter();
+
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+  router,
+}).$mount('#app');
